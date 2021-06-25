d746aee1ae6d556e2bf56f51a9752df103dd22a9
SIC-brunorocha <68641905+SIC-brunorocha@users.noreply.github.com>
2021-06-25 16:37:51 +0100

    portal
    portal

---
+++ SIC-Portal/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    "name": "SIC Portal Costumer",
+    "version": "1.0",
+    "category": "Extra",
+    "summary": "Modify the Portal customer view to add more status",
+
+    "depends": ['account', 'delivery'],
+
+    'data': [
+
+    ],
+
+    'images': ,
+
+    'qweb': [
+       
+    ],
+
+    "author": "SIC-Smart IMprove",
+    "website": "https://smartimproveconsulting.com",
+    'support': 'support@tsmartimprove.pt',
+    'maintainer': 'SIC-Bruno',
+
+    "description": """
+        """,
+
+    'demo': [],
+    'license': 'OPL-1',
+    'live_test_url': '',
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+    "price": "0",
+    "currency": "EUR",
+}
